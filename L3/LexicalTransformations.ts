6ee093a4705fa919d098ec61fd1e34b4db07900f
arieljay <arieljay@post.bgu.ac.il>
2024-06-17 15:49:06 +0300

    finishing 2c: Lexicaltransformation and 2.3 asiigngment
    @todo: finish tests
    
    Co-authored-by: Yuval Nachman <yuviwx@users.noreply.github.com>

--- L3/LexicalTransformations.ts
+++ L3/LexicalTransformations.ts
@@ -1,5 +1,5 @@
-import { ClassExp, ProcExp, Exp, Program, IfExp } from "./L3-ast";
-import { Result, makeFailure } from "../shared/result";
+import { ClassExp, ProcExp, Exp, Program, IfExp, isExp, isCExp, isDefineExp, isIfExp, isLitExp, makeDefineExp, isAtomicExp, isAppExp, isProcExp, isLetExp, isProgram, makeProgram, makeLetExp, makeBinding, isClassExp, makeLitExp } from "./L3-ast";
+import { Result, makeFailure, makeOk, mapResult, mapv } from "../shared/result";
 import { CExp, makeProcExp, Binding, makeAppExp, makeVarDecl, makePrimOp, makeIfExp, makeBoolExp, makeVarRef} from "./L3-ast";
 
 /*
@@ -20,12 +20,10 @@ export const class2proc = (exp: ClassExp): ProcExp =>
         i === methods.length ?  
             makeBoolExp(false) :
             makeIfExp(
-                makeAppExp(makePrimOp("="),[makeVarRef("msg"), makeVarRef(methods[i].var.var)]),
+                makeAppExp(makePrimOp("eq?"),[makeVarRef("msg"), makeLitExp({ tag: "SymbolSExp", val: methods[i].var.var })]),
                 methods[i].val,
                 makeConditions(methods,i+1)
             ) 
-
-
 
 
 /*
@@ -34,6 +32,23 @@ Signature: lexTransform(AST)
 Type: [Exp | Program] => Result<Exp | Program>
 */
 
-export const lexTransform = (exp: Exp | Program): Result<Exp | Program> =>
-    //@TODO
-    makeFailure("ToDo");
+export const lexTransform = (exp: Exp | Program): Result<Exp | Program> => 
+    isExp(exp) ? makeOk(rewriteAllClassExp(exp)) : 
+    isProgram(exp) ? makeOk(makeProgram(exp.exps.map(rewriteAllClassExp))) :
+    makeFailure(exp);
+
+const rewriteAllClassExp = (exp: Exp): Exp =>
+    isCExp(exp) ? rewriteAllClassCExp(exp) :
+    isDefineExp(exp) ? makeDefineExp(exp.var, rewriteAllClassCExp(exp.val)) :
+    exp;
+
+const rewriteAllClassCExp = (exp: CExp): CExp =>
+    isAtomicExp(exp) ? exp :
+    isLitExp(exp) ? exp :
+    isIfExp(exp) ? makeIfExp(rewriteAllClassCExp(exp.test), rewriteAllClassCExp(exp.then), rewriteAllClassCExp(exp.alt)) :
+    isAppExp(exp) ? makeAppExp(rewriteAllClassCExp(exp.rator), exp.rands.map(rewriteAllClassCExp)) :
+    isProcExp(exp) ? makeProcExp(exp.args, exp.body.map(rewriteAllClassCExp)) :
+    isLetExp(exp) ? makeLetExp(exp.bindings.map((b : Binding) : Binding => 
+                                            makeBinding(b.var.var, rewriteAllClassCExp(b.val))), exp.body.map(rewriteAllClassCExp)) :
+    isClassExp(exp) ? class2proc(exp) :
+    exp;
